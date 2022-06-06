0536d5405b49401dddbeaa7a8477aac49910048f
BenC0 <suddensquirrel94@gmail.com>
2022-06-06 09:52:44 +0100

    Commented delivery method preselection and removed unneccessary function

--- src/checkout_delivery_preselection/index.js
+++ src/checkout_delivery_preselection/index.js
@@ -1,5 +1,6 @@
 import { elementManagement, log } from "../norman";
 
+// resets and applies the active classes to the desired fullfillment option
 export function make_selection(preference = "none") {
     let active_els = elementManagement.getAll(`.checkout-delivery__location-label.active, .checkout-delivery__location-label.active .active, .deliverySelectionView .show, #continueToPayment`)
     active_els.forEach(el => el.classList.remove("active", "show"))
@@ -25,33 +26,9 @@ export function make_selection(preference = "none") {
     }
 }
 
+// evaluates if the checkout is valid for the test or not.
 export function checkout_is_valid() {
     return !elementManagement.exists(`.checkout-delivery__location-label.disabled`) && elementManagement.exists(`#newOrderSummaryTotalHiddenForCnC`) && elementManagement.exists(`#orderSummaryTotal`)
-}
-
-export function update_summary() {
-    let summary_el = elementManagement.get("#orderSummaryTotal")
-    summary_el = summary_el.pop()
-
-    let cnc_summary_el = elementManagement.get("#newOrderSummaryTotalHiddenForCnC")
-    log({cnc_summary_el})
-    cnc_summary_el = cnc_summary_el[0]
-    log({cnc_summary_el})
-    log(cnc_summary_el.value)
-    let new_str = cnc_summary_el.value
-    log({new_str})
-    new_str = new_str.replace(/Click (&|&amp;) Collect/g, "").replace(/,\s,/g, ",")
-    log({new_str})
-    summary_el.innerHTML = new_str
-
-    let els_to_hide = elementManagement.getAll("#isShowDeliverySection, #ordSummaryDeliveryOptionSelected")
-    els_to_hide.forEach(e => e.parentNode.style.display = "none")
-
-    elementManagement.getAll(".checkout-delivery__location-label").forEach(el => {
-        el.addEventListener("click", _ => {
-            els_to_hide.forEach(e => e.parentNode.style.display = "")
-        })
-    })
 }
 
 export const checkout = {
